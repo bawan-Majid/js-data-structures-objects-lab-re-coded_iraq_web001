4b9d14cf32f9efdfb94b3e525693573b5a354e38
Bawan Majid <bawanmajid23@gmail.com>
2020-06-03 07:34:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your solution in this file!
+let driver={key:"hi"};
+function updateDriverWithKeyAndValue(driver,key,value){
+  driver['key']=value;
+ return driver 
+}
+function destructivelyUpdateDriverWithKeyAndValue(driver,key){
+  return driver
+}
+function deleteFromDriverByKey( driver,key){
+delete driver['key'];
+}
+
+updateDriverWithKeyAndValue(driver,key,'11 Broadway');
+destructivelyUpdateDriverWithKeyAndValue(driver,key);
